ebeffb2137a0d8e29b61417240832b517a89ddff
ehsan <ehsan.shirzadi@gmail.com>
2022-10-16 17:18:57 +0330

    updated: only a simple command needed

--- build.sh
+++ build.sh
@@ -1,10 +1,1 @@
-docker rmi newman  || true
-docker build -t newman -f Dockerfile .
-docker stop newman || true
-docker rm newman || true
-docker run --name newman -d --restart always --network="host" newman
-#docker run --name newman -d --restart always --collection="swapp.postman_collection.json" newman
-
-#docker run -v ~/collections:/etc/postman -t postman/newman_alpine33 \
-#    --collection="swapp.postman_collection.json" \
-#    --html="newman-results.html"
+docker run -v $PWD/collections/:/etc/newman/ --network="host" -t postman/newman:alpine run swapp.postman_collection.json
